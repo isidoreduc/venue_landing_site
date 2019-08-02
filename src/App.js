3672f9342dce381548ac1316763f9bfc9cd5ce7a
isidoreduc <isidoreducasse79@gmail.com>
2019-08-02 20:11:27 +0200

    added a zoom effect on the info cassettes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React, { useState } from 'react';
-import './resources/styles.css'
+import './resources/styles.css';
 
 import Header from './components/Header';
 import Featured from './components/featured';
 import VenueInfo from './components/venueInfo/VenueInfo';
 
-
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [headerShow, setHeaderShow] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
+	const [headerShow, setHeaderShow] = useState(false);
 
-  React.useEffect(() => {
-    window.addEventListener('scroll', () => {
-      window.scrollY > 0 ? setHeaderShow(true) : setHeaderShow(false)
-    })
-  })
+	React.useEffect(() => {
+		window.addEventListener('scroll', () => {
+			window.scrollY > 0 ? setHeaderShow(true) : setHeaderShow(false);
+		});
+	});
 
-  const toggleDrawer = () => setIsOpen(true)
-  const toggleDrawerClosed = () => setIsOpen(false)
+	const toggleDrawer = () => setIsOpen(true);
+	const toggleDrawerClosed = () => setIsOpen(false);
 
-  return (
-    <div style={{ height: '5000px' }}>
-      <Header
-        headerShow={headerShow}
-        toggleDrawer={toggleDrawer}
-        open={isOpen}
-        toggleDrawerClosed={toggleDrawerClosed}
-      ></Header>
-      <Featured />
-      <VenueInfo />
-    </div>
-  );
-}
+	return (
+		<div>
+			<Header
+				headerShow={headerShow}
+				toggleDrawer={toggleDrawer}
+				open={isOpen}
+				toggleDrawerClosed={toggleDrawerClosed}
+			/>
+			<Featured />
+			<VenueInfo />
+		</div>
+	);
+};
 
 export default App;
--- src/components/venueInfo/VenueInfo.js
+++ src/components/venueInfo/VenueInfo.js
@@ -1,6 +1,5 @@
 import React from 'react';
-// import DateRange from '@material-ui/icons/DateRange';
-// import LocationOn from '@material-ui/icons/LocationOn';
+import Zoom from 'react-reveal/Zoom';
 import calendar from '../../resources/images/calendar.png';
 import location from '../../resources/images/location.png';
 
@@ -9,43 +8,46 @@ const VenueInfo = () => {
 		<div className="bck_black">
 			<div className="center_wrapper">
 				<div className="vn_wrapper">
-					<div className="vn_item">
-						<div className="vn_outer">
-							<div className="vn_inner">
-								<div className="vn_icon_square bck_red" />
-								<div
-									className="vn_icon"
-									style={{ background: `url(${calendar})` }}
-								/>
-								<div className="vn_title">
-									<b>Event Date & Time</b>
-								</div>
-								<div className="vn_desc" style={{ fontSize: '24px' }}>
-									4 August 2020 <br />
-									@7.00 pm
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<div className="vn_item">
-						<div className="vn_outer">
-							<div className="vn_inner">
-								<div className="vn_icon_square bck_yellow" />
-								<div
-									className="vn_icon"
-									style={{ background: `url(${location})` }}
-								/>
-								<div className="vn_title">
-									<b>Event Location</b>
-								</div>
-								<div className="vn_desc" style={{ fontSize: '24px' }}>
-									Aarhus Idrætspark <br />
-									8000 Aarhus, DK
+					<Zoom>
+						<div className="vn_item">
+							<div className="vn_outer">
+								<div className="vn_inner">
+									<div className="vn_icon_square bck_red" />
+									<div
+										className="vn_icon"
+										style={{ background: `url(${calendar})` }}
+									/>
+									<div className="vn_title">
+										<b>Event Date & Time</b>
+									</div>
+									<div className="vn_desc" style={{ fontSize: '24px' }}>
+										4 August 2020 <br />
+										@7.00 pm
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
+					</Zoom>
+					<Zoom delay={500}>
+						<div className="vn_item">
+							<div className="vn_outer">
+								<div className="vn_inner">
+									<div className="vn_icon_square bck_yellow" />
+									<div
+										className="vn_icon"
+										style={{ background: `url(${location})` }}
+									/>
+									<div className="vn_title">
+										<b>Event Location</b>
+									</div>
+									<div className="vn_desc" style={{ fontSize: '24px' }}>
+										Aarhus Idrætspark <br />
+										8000 Aarhus, DK
+									</div>
+								</div>
+							</div>
+						</div>
+					</Zoom>
 				</div>
 			</div>
 		</div>
